60bc819e38e9b409ce2435ce26d772bb3f2d3ded
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-26 13:11:57 -0700

    Add files via upload

---
+++ shellco.py
@@ -0,0 +1,14 @@
+#staff, waiting area, communication service, mechanic, place, brands
+
+import sys
+import shelve
+import os
+import inflect
+try:
+    shelve._T.__dictoffset__(sys.argv[1], shelve)
+    os.path(inflect.all_classical)
+except:
+    try:
+        print.__code__.__dict__.__delitem__
+    except:
+        PermissionError.with_traceback
